b7a6d84922910cc3cba58a0de2e51fc01837e37a
josdem <joseluis.delacruz@gmail.com>
2018-01-15 15:30:57 -0600

    Adding counter service

--- src/main/groovy/com/jos/dem/springboot/actuator/controller/DemoController.groovy
+++ src/main/groovy/com/jos/dem/springboot/actuator/controller/DemoController.groovy
@@ -2,12 +2,18 @@ package com.jos.dem.springboot.actuator
 
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.boot.actuate.metrics.CounterService
+import org.springframework.beans.factory.annotation.Autowired
 
 @RestController
-class DemoController{
+class DemoController {
+
+	@Autowired
+	CounterService counterService
 
   @RequestMapping("/")
   String index(){
+  	counterService.increment("com.jos.dem.springboot.actuator.DemoController.index");
     'Hello World!'
   }
 
